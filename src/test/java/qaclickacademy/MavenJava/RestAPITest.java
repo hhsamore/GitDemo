3a517ed55844349e536ca19806495482ddec9c2d
hammad <hhsamore@gmail.com>
2020-12-28 13:19:36 +0100

    first commit

---
+++ src/test/java/qaclickacademy/MavenJava/RestAPITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void postJIRA()
+	{
+		System.out.println("PostJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+}
